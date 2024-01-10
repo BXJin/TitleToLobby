d3f650b36e1c02276c4bd6b01fdd795305197a10
BXJin <bsung472@gmail.com>
2024-01-10 13:31:46 +0900

    Title2Lobby FirstCommit

---
+++ Source/Title2Lobby/Public/Title/CodeTitleHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CodeTitleHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TITLE2LOBBY_API ACodeTitleHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
